c0315c5ec9b9db05d68b71166919f1114f1a957d
Maxim Veksler <maxim@vekslers.org>
2025-04-08 16:42:43 +0300

    Catch js Errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/react";
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Initialize Sentry
+Sentry.init({
+dsn: "https://4d89d85b3b4b514ba02d29c1d5d49dce@o4509117180346368.ingest.de.sentry.io/4509117190897744"
+});
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <Sentry.ErrorBoundary fallback={<p>אירעה שגיאה בטעינת האפליקציה. אנא נסה שוב מאוחר יותר.</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
+  );
+}
