2cd91da1e3d99b8aacdbaa07227d965bb9716fe9
Maxim Veksler <maxim@vekslers.org>
2025-04-08 15:46:41 +0300

    integrate sentry #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+    chunkSizeWarningLimit: 10000, // Increa§se chunk size warning limit
+  },
   server: {
     host: "::",
     port: 8080,
@@ -13,6 +18,12 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    mode !== 'development' && sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "55b30fd4159e",
+      project: "meirim-likud-form",
+      telemetry: false
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
